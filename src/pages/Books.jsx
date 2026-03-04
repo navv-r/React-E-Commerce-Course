3d27392e063a83ebe570419a201e7f41d4b8a3c2
navv-r <navendra604@gmail.com>
2026-03-04 10:39:35 -0500

    filtering for books done

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -3,8 +3,37 @@ import Book from "../components/UI/Book";
 
 const Books = ({ books: initialBooks }) => {
   const [books, setBooks] = useState(initialBooks);
+
+  function filterBooks(filter) {
+    console.log(filter);
+    if (filter === "LOW_TO_HIGH") {
+      setBooks(
+        books
+          .slice()
+          .sort(
+            (a, b) =>
+              (a.salePrice || a.originalPrice) -
+              (b.salePrice || b.originalPrice),
+          ),
+      );
+    }
+    if (filter === "HIGH_TO_LOW") {
+      setBooks(
+        books
+          .slice()
+          .sort(
+            (a, b) =>
+              (b.salePrice || b.originalPrice) -
+              (a.salePrice || a.originalPrice),
+          ),
+      );
+    }
+    if (filter === "RATING") {
+      setBooks(books.slice().sort((a, b) => b.rating - a.rating));
+    }
+  }
   return (
-    <div id="books__body">  
+    <div id="books__body">
       <main id="books__main">
         <section>
           <div className="books__container">
@@ -13,8 +42,12 @@ const Books = ({ books: initialBooks }) => {
                 <h2 className="section__title books__header--title">
                   All Books{" "}
                 </h2>
-                <select id="filter">
-                  <option value="" selected disabled>
+                <select
+                  id="filter"
+                  defaultValue="DEFAULT"
+                  onChange={(event) => filterBooks(event.target.value)}
+                >
+                  <option value="" disabled>
                     Sort
                   </option>
                   <option value="LOW_TO_HIGH">Price Low to High</option>
@@ -23,10 +56,10 @@ const Books = ({ books: initialBooks }) => {
                 </select>
               </div>
               <div className="books">
-                {books.map((book) => ( 
+                {books.map((book) => (
                   <Book book={book} key={book.id} />
                 ))}
-              </div> 
+              </div>
             </div>
           </div>
         </section>
